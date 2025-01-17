1939d69f642bea8e60a672df018437e6affcd919
Caleb Buckley <cbuckley021@icloud.com>
2025-01-18 11:58:38 +1300

    Added Dockerfiles, created postgres and java images, edited pom.xml to copy dependencies to build to prevent having to build uber jar every time theres a change during development, Docker images run OK

--- dev_java/dev_java/src/main/java/com/buckl3y/GPSProducer.java
+++ dev_java/dev_java/src/main/java/com/buckl3y/GPSProducer.java
@@ -16,10 +16,10 @@ public class GPSProducer {
     public List<String> generateSerialNumbers(int snCount) {
         int number = 1;
         List<String> list = new ArrayList<>();
-        for(int i = 0; i <= snCount; i++) {
+        for (int i = 0; i <= snCount; i++) {
             StringBuilder ranString = new StringBuilder();
-            //generate random 5-byte string
-            for(int j = 0; j < 5; j++) {
+            // generate random 5-byte string
+            for (int j = 0; j < 5; j++) {
                 int index = rand.nextInt(library.length());
                 ranString.append(library.charAt(index));
             }
@@ -33,25 +33,26 @@ public class GPSProducer {
 
     public void refreshCoordinates(List<String> list) {
         ZonedDateTime time = ZonedDateTime.now();
-        for(String sn : list) {
+        for (String sn : list) {
             Double randLongitude = rand.nextDouble() * 180;
             Double randLatitude = rand.nextDouble() * 180;
-            randLongitude = (double)Math.round(randLongitude * 10000000) / 10000000;
-            randLatitude = (double)Math.round(randLatitude * 10000000) / 10000000;
+            randLongitude = (double) Math.round(randLongitude * 10000000) / 10000000;
+            randLatitude = (double) Math.round(randLatitude * 10000000) / 10000000;
             messages.add(new Message(sn, time, randLatitude, randLongitude));
         }
     }
 
     public void sendMessages() {
-        //send JSON messages to RabbitMQ
-        if(!messages.isEmpty()) {
-            for(Message message : messages) {
+        // send JSON messages to RabbitMQ
+        if (!messages.isEmpty()) {
+            for (Message message : messages) {
                 JSONObject json = new JSONObject();
                 try {
-                    json.put("serialNumber" , message.getSerialNumber());
+                    json.put("serialNumber", message.getSerialNumber());
                     json.put("time", message.getTime());
                     json.put("latitude", message.getLatitude());
                     json.put("longitude", message.getLongitude());
+                    System.out.println(json.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -59,5 +60,3 @@ public class GPSProducer {
         }
     }
 }
-
-
